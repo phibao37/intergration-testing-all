7e798a3a9275d50c83baad8bb4386e85a550f493
Sầm Đức Vũ <phibao37@hotmail.com>
2015-07-08 15:42:24 +0700

    them jdt

--- Integration Testing v2/src/jdt/JUnitVisitor.java
+++ Integration Testing v2/src/jdt/JUnitVisitor.java
@@ -3,16 +3,41 @@ package jdt;
 import java.io.File;
 import java.util.ArrayList;
 
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+
 import core.models.Function;
 import core.models.Variable;
 import core.visitor.UnitVisitor;
 
 public class JUnitVisitor implements UnitVisitor {
-
+	
+	private ArrayList<Function> mFunctions = new ArrayList<Function>();
+	
 	@Override
 	public UnitVisitor parseSource(String source, File file, Object... args) {
-		// TODO Auto-generated method stub
-		return null;
+		mFunctions.clear();
+		
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
+		parser.setSource(source.toCharArray());
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+ 
+		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+		cu.accept(new ASTVisitor() {
+
+			@Override
+			public boolean visit(MethodDeclaration node) {
+				String name = node.getName().getIdentifier();
+				return false;
+			}
+			 
+			
+			
+		});
+		return this;
 	}
 
 	@Override
@@ -23,8 +48,8 @@ public class JUnitVisitor implements UnitVisitor {
 
 	@Override
 	public ArrayList<Variable> getGlobalVariableList() {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO Lam sau
+		return new ArrayList<Variable>();
 	}
 
 }
