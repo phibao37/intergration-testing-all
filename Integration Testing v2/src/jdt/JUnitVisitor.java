d2691b2fc99f018b93b8e329f996cc338270a8a3
Sầm Đức Vũ <phibao37@hotmail.com>
2015-07-08 15:23:17 +0700

    Them cac file jar vao muc dong bo

---
+++ Integration Testing v2/src/jdt/JUnitVisitor.java
@@ -0,0 +1,30 @@
+package jdt;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import core.models.Function;
+import core.models.Variable;
+import core.visitor.UnitVisitor;
+
+public class JUnitVisitor implements UnitVisitor {
+
+	@Override
+	public UnitVisitor parseSource(String source, File file, Object... args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Function> getFunctionList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Variable> getGlobalVariableList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
