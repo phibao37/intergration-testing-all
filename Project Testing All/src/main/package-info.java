d28720a666091ef5dd3a27136666a1c0f33dfd0f
Sầm Đức Vũ <phibao37@hotmail.com>
2016-10-21 17:23:52 +0700

    [all] Add default eclipse configuration

---
+++ Project Testing All/src/main/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Package info declaration file.
+ * @file package-info.java
+ * @author (SDV)[VuSD]
+ * Copyright (C) 2016 SDV, All Rights Reserved.
+ */
+
+/**
+ * Package contains class to launch the main application GUI
+ * @author VuSD
+ */
+package main;
